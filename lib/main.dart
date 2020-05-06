b88bd05ece0e26e57d7c36cb4be4a483b5765f22
sahanisantosh <techquans@gmail.com>
2020-05-06 16:51:19 +0530

    Magic Ball Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Magic Ball'),
+          ),
+          backgroundColor: Colors.lightBlue,
+          body: Ball(),
+        ),
+      ),
+    );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image(
+            image: AssetImage('images/ball$ballNumber.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
